81954e4f701018c2248ea15f99865a63f86f5c7c
Rose Bowen <rosebowen1997@gmail.com>
2023-09-14 17:20:34 +0100

    mute/unmute homepage video

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,12 +1,26 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Home() {
+  const [unMute, setunMute] = useState(false);
+
   return (
-    <video
-      className="object-cover h-full w-full"
-      muted
-      autoPlay
-      src="/assets/harry-grad-movie.mp4"
-      loop
-      playsInline
-    />
+    <>
+      <video
+        className="object-cover h-full w-full"
+        autoPlay
+        src="/assets/harry-grad-movie.mp4"
+        loop
+        playsInline
+        muted={!unMute}
+      />
+      <button
+        className="text-white absolute bottom-10 right-5 md:right-10"
+        onClick={() => setunMute(!unMute)}
+      >
+        {(unMute ? "" : "un-") + "mute"}
+      </button>
+    </>
   );
 }
